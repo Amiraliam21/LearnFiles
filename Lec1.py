1f10e1f6adc366961ae268c75966e2c4ad9ab24d
HazaraLearnHub <87286815+Amiraliam21@users.noreply.github.com>
2022-12-08 23:38:44 +0100

    lets beginn with Data Analystics
    I'm so excited

---
+++ Lec1.py
@@ -0,0 +1,8 @@
+''' What is Data Analytics?
+
+a process of cleansing, inspecting, transforming, and modeling data with the goal of discovering useful information, informing conclusion and
+supporting decision-making.
+
+
+
+'''
